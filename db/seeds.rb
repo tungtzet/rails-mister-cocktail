d8e7fc1a91fc33c8aa8a5763769910cbfbe9b392
tung nguyen <tungnt@arena.edu.vn>
2020-07-30 12:04:39 +0200

    db:seed ingredients done

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'open-uri'
+
+serialized_ingredients = open('https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list').read
+ingredients = JSON.parse(serialized_ingredients)["drinks"]
+
+ingredients.each do |ingredient|
+  Ingredient.create(name: ingredient["strIngredient1"])
+end
